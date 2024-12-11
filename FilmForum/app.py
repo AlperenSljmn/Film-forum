c873b66df8160e13f96dc8278c8e27b78607ce45
Alperen <alperensulejman45@gmail.com>
2024-12-11 14:37:53 +0100

    Update app.py

--- FilmForum/app.py
+++ FilmForum/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
