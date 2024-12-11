a34559a6e8906b31cedfb8df089d86b32e8f6d8b
Alperen <alperensulejman45@gmail.com>
2024-12-11 13:46:37 +0100

    Update app.py

--- FilmForum/app.py
+++ FilmForum/app.py
@@ -114,7 +114,8 @@ def create_topic():
 
 # Uygulama başlatıldığında veritabanını oluştur
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    
